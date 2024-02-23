65d106294b4dc3d0c20f57040d2fd233bb56ca48
Gabriel Oporto <gabyoporto87@gmail.com>
2024-02-23 11:21:18 -0300

    update config

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export async function GET(context) {
+  const posts = await getCollection("posts");
+  return rss({
+    title: "Gabriel Oporto | Blog",
+    description: "Todo acerca de las ultimas tecnologias Frontend",
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
+    customData: `<language>es-ar</language>`,
+  });
+}
